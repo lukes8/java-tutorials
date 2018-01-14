562591ebb74b1a3bb7bea2ca1d03a710030e2426
Lukas <lukas.sipinka@cgi.com>
2018-01-14 19:53:24 +0100

    Added initial regex expressions

--- regular_expressions/src/main/java/cz/lusi/App.java
+++ regular_expressions/src/main/java/cz/lusi/App.java
@@ -11,6 +11,8 @@ public class App
 {
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9.-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_NAME_TEXT_REGEX =
+            Pattern.compile("^[a-zA-Z]+(\\s*\\w*)*\\(\\w+\\)$", Pattern.CASE_INSENSITIVE);
 
     public static void main( String[] args )
     {
@@ -20,10 +22,21 @@ public class App
             System.out.println("Unfortunately, your entered email is not valid!");
         }
 
+        if (validateName("Alexander Abran (ALEB)")) {
+            System.out.println("Yeah, your entered name with abbrevitation is valid!");
+        } else {
+            System.out.println("Unfortunately, you must enter name in valid format: Name (abbrevitation)!");
+        }
+
     }
 
     public static boolean validateEmail(String email) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
+        return matcher.find();
+    }
+
+    public static boolean validateName(String name) {
+        Matcher matcher = VALID_NAME_TEXT_REGEX.matcher(name);
         return matcher.find();
     }
 }
